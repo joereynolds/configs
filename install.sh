e6848a4185303799648e1881f39c10288e44d271
Joe Reynolds <joe.reynolds@sykescottages.co.uk>
2016-02-23 08:25:33 +0000

    update shellscript and readme

--- install.sh
+++ install.sh
@@ -20,3 +20,22 @@ for package in ${packages[*]}; do
 done
 echo "Packages installed."
 
+echo "Installing Vim plugins started"
+
+echo "Installing Command-T"
+git clone https://github.com/wincent/command-t
+cd ~/.vim/bundle/command-t
+rake make
+
+echo "Installing Syntastic"
+git clone https://github.com/scrooloose/syntastic.git
+
+echo "Installing Tagbar"
+git clone https://github.com/majutsushi/tagbar
+
+echo "Installing YouCompleteMe"
+git clone https://github.com/valloric/youcompleteme
+cd youcompleteme
+git submodule update --init --recursive
+./install.py --clang-completer
+
