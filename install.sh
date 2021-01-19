6920b38d8e6a1659d00ac3f8f5725cb1cbeb9ca6
Joe Reynolds <joereynolds952@gmail.com>
2021-01-19 12:42:25 +0000

    Use stow where possible

--- install.sh
+++ install.sh
@@ -18,6 +18,7 @@ packagelist=(
   npm 
   pavucontrol 
   simplescreenrecorder 
+  stow
   suckless-tools 
   xclip 
   xdotool 
@@ -27,34 +28,22 @@ sudo apt-get install ${packagelist[@]}
 
 sudo snap install --classic ripgrep
 
-
-
-
-
 # Install fd
 wget https://github.com/sharkdp/fd/releases/download/v8.2.1/fd_8.2.1_amd64.deb
 sudo dpkg -i fd_8.2.1_amd64.deb
 rm fd_8.2.1_amd64.deb
 
 # Install z
-wget https://raw.githubusercontent.com/rupa/z/master/z.sh
-mv ./z.sh ~/z.sh
+wget -O ~/z.sh https://raw.githubusercontent.com/rupa/z/master/z.sh
 
 # Install dbeaver
 wget https://github.com/dbeaver/dbeaver/releases/download/7.3.3/dbeaver-ce_7.3.3_amd64.deb
 sudo dpkg -i dbeaver-ce_7.3.3_amd64.deb
 rm dbeaver-ce_7.3.3_amd64.deb
 
-
-rm -f ~/.gitconfig ~/.bashrc ~/.config/i3/config ~/.config/nvim/init.vim ~/bin/keys ~/bin/todo
-
-ln -sr ./files/dotfiles/.gitconfig ~/.gitconfig
-ln -sr ./files/i3/config ~/.config/i3/config
-ln -sr ./files/nvim/init.vim ~/.config/nvim/init.vim
-ln -sr ./files/dotfiles/.bashrc ~/.bashrc
-
-ln -sr ./files/dmenu-scripts/keys ~/bin/keys
-ln -sr ./files/dmenu-scripts/todo ~/bin/todo
+cd ./dotfiles
+stow bash git i3 nvim dmenu-scripts -t ~/
+cd -
 
 # Install and run vim plug
 curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
