03d16f4acfc21415ba6ee489a52e42ec6709b277
Joe Reynolds <joereynolds952@gmail.com>
2021-01-19 13:06:17 +0000

    Save .deb's to /tmp

--- install.sh
+++ install.sh
@@ -29,17 +29,16 @@ sudo apt-get install ${packagelist[@]}
 sudo snap install --classic ripgrep
 
 # Install fd
-wget https://github.com/sharkdp/fd/releases/download/v8.2.1/fd_8.2.1_amd64.deb
-sudo dpkg -i fd_8.2.1_amd64.deb
-rm fd_8.2.1_amd64.deb
+wget -O /tmp/fd.deb https://github.com/sharkdp/fd/releases/download/v8.2.1/fd_8.2.1_amd64.deb
+sudo dpkg -i /tmp/fd.deb
+
+# Install dbeaver
+wget -O /tmp/dbeaver.deb https://github.com/dbeaver/dbeaver/releases/download/7.3.3/dbeaver-ce_7.3.3_amd64.deb
+sudo dpkg -i dbeaver.deb
 
 # Install z
 wget -O ~/z.sh https://raw.githubusercontent.com/rupa/z/master/z.sh
 
-# Install dbeaver
-wget https://github.com/dbeaver/dbeaver/releases/download/7.3.3/dbeaver-ce_7.3.3_amd64.deb
-sudo dpkg -i dbeaver-ce_7.3.3_amd64.deb
-rm dbeaver-ce_7.3.3_amd64.deb
 
 cd ./dotfiles
 stow bash git i3 nvim dmenu-scripts -t ~/
