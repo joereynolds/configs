8279ffe24d26ebafc267818e535397b1b6f221e0
Joe Reynolds <joereynolds952@gmail.com>
2021-05-19 14:23:03 +0100

    Update install for Manjaro

--- install.sh
+++ install.sh
@@ -1,59 +1,28 @@
-# Assumes Ubuntu 20.04
-
-# Ubuntu likes to have its own .bashrc file (rudely) present, remove it.
+# Assumes Manjaro
 rm -f ~/.bashrc
 
-packagelist=(
-  arandr 
-  composer 
-  curl 
-  dunst
-  ffmpeg
-  flameshot
-  fzy 
-  git  
-  gparted
-  htop 
-  i3-wm 
-  i3lock 
-  i3status 
-  meld 
-  moreutils 
-  nodejs 
-  npm 
-  pavucontrol 
-  rofi
-  simplescreenrecorder 
-  stow
-  suckless-tools 
-  tree
-  vlc
-  xclip 
-  xdotool 
-)
+sudo pacman -S --needed base-devel git
+git clone https://aur.archlinux.org/yay-git.git
+cd yay
+makepkg -si
 
-sudo apt-add-repository ppa:maarten-baert/simplescreenrecorder
-sudo apt-get update
-
-sudo apt-get install ${packagelist[@]}
-
-sudo snap install --classic ripgrep
-sudo snap install spotify
-
-# Install fd
-wget -O /tmp/fd.deb https://github.com/sharkdp/fd/releases/download/v8.2.1/fd_8.2.1_amd64.deb
-sudo dpkg -i /tmp/fd.deb
-
-# Install dbeaver
-wget -O /tmp/dbeaver.deb https://github.com/dbeaver/dbeaver/releases/download/7.3.3/dbeaver-ce_7.3.3_amd64.deb
-sudo dpkg -i dbeaver.deb
-
-# Install z
-wget -O ~/z.sh https://raw.githubusercontent.com/rupa/z/master/z.sh
-
-# Install neovim
-wget -O /usr/local/bin/nvim https://github.com/neovim/neovim/releases/download/v0.4.4/nvim.appimage
-chmod a+x /usr/local/bin/nvim
+yay -S dbeaver
+yay -S fd
+yay -S flameshot
+yay -S fzy
+yay -S google-chrome
+yay -S neovim
+yay -S node
+yay -S tiled
+yay -S ripgrep
+yay -S simplescreenrecorder
+yay -S spotify
+yay -S steam
+yay -S stow
+yay -S tree
+yay -S yabridge
+yay -S yabridgectl
+yay -S z
 
 # Symlink our configs
 cd ./dotfiles
@@ -63,3 +32,20 @@ cd -
 # Install and run vim plug
 curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 nvim +PlugInstall +qall > /dev/null
+
+# TODO - 
+# packagelist=(
+#   arandr 
+#   composer 
+#   dunst
+#   i3-wm 
+#   i3lock 
+#   i3status 
+#   meld 
+#   moreutils 
+#   pavucontrol 
+#   rofi
+#   suckless-tools 
+#   xclip 
+#   xdotool 
+# )
