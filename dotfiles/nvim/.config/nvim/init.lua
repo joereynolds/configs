bfabd57ee7b83bb6e934659ef7eac3adc3008d50
Joe Reynolds <joe.reynolds@sykescottages.co.uk>
2024-05-21 10:09:14 +0100

    Remove more defaults after nvim 0.10 release

--- dotfiles/nvim/.config/nvim/init.lua
+++ dotfiles/nvim/.config/nvim/init.lua
@@ -7,7 +7,6 @@ vim.call('plug#begin')
     Plug 'tpope/vim-commentary'
     Plug 'tpope/vim-fugitive'
     Plug 'stefandtw/quickfix-reflector.vim'
-    Plug 'Lokaltog/vim-monotone'
 vim.call('plug#end')
 
 vim.opt.expandtab = true
@@ -15,7 +14,6 @@ vim.opt.ignorecase = true
 vim.opt.inccommand = 'split' --Live substitution is the bees knees
 vim.opt.shiftwidth = 4 -- indentation should be 4 spaces when we use >> and <<
 vim.opt.swapfile = false
-vim.opt.grepprg = 'rg --vimgrep --ignore-case'
 
 vim.g.loaded_matchparen = 1 --Crazy slow and annoying highlighting
 vim.g.loaded_netrwPlugin = 1 -- https://github.com/vim/vim/issues/5073
@@ -44,6 +42,5 @@ vim.keymap.set('i', ';', '<esc>mzA;<esc>`z')
 vim.keymap.set('t', '<esc>', '<c-\\><c-n>')
 vim.keymap.set('n', 'K', ':call CocAction("doHover")<cr>')
 
-vim.cmd('colorscheme monotone')
 vim.cmd('packadd cfilter')
 vim.cmd("call coc#add_extension('coc-json', 'coc-git', 'coc-phpls', 'coc-css', 'coc-html')")
