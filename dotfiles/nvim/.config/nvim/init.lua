ead2bc2993ce6e3af0ad6bc236d169c587df08a7
Joe Reynolds <joe.reynolds@sykescottages.co.uk>
2024-07-31 14:21:57 +0100

    Add oil to nvim

--- dotfiles/nvim/.config/nvim/init.lua
+++ dotfiles/nvim/.config/nvim/init.lua
@@ -6,16 +6,16 @@ vim.call('plug#begin')
     Plug 'srstevenson/vim-picker'
     Plug 'tpope/vim-fugitive'
     Plug 'stefandtw/quickfix-reflector.vim'
+    Plug 'stevearc/oil.nvim'
 vim.call('plug#end')
+
+require("oil").setup()
 
 vim.opt.expandtab = true
 vim.opt.ignorecase = true
 vim.opt.inccommand = 'split' --Live substitution is the bees knees
 vim.opt.shiftwidth = 4 -- indentation should be 4 spaces when we use >> and <<
 vim.opt.swapfile = false
-
-vim.g.loaded_matchparen = 1 --Crazy slow and annoying highlighting
-vim.g.loaded_netrwPlugin = 1 -- https://github.com/vim/vim/issues/5073
 
 vim.keymap.set('n', ']b', ':bnext<cr>')
 vim.keymap.set('n', '[b', ':bprevious<cr>')
@@ -28,6 +28,7 @@ vim.keymap.set('n', '[q', ':cprevious<cr>')
 vim.keymap.set('i', ';', '<esc>mzA;<esc>`z')
 vim.keymap.set('t', '<esc>', '<c-\\><c-n>')
 vim.keymap.set('n', 'K', ':call CocAction("doHover")<cr>')
+vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
 
 vim.cmd('packadd cfilter')
 vim.cmd("call coc#add_extension('coc-json', 'coc-git', 'coc-phpls', 'coc-css', 'coc-html')")
