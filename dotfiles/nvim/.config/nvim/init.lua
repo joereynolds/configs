33d1ce4b444224b17caebfbea461f82c54ee76e4
Joe Reynolds <joe.reynolds@sykescottages.co.uk>
2025-03-26 16:07:46 +0000

    Remove mappings that are now default in nvim

--- dotfiles/nvim/.config/nvim/init.lua
+++ dotfiles/nvim/.config/nvim/init.lua
@@ -20,18 +20,13 @@ vim.opt.inccommand = 'split' --Live substitution is the bees knees
 vim.opt.shiftwidth = 4 -- indentation should be 4 spaces when we use >> and <<
 vim.opt.swapfile = false
 
-vim.keymap.set('n', ']b', ':bnext<cr>')
-vim.keymap.set('n', '[b', ':bprevious<cr>')
 vim.keymap.set('n', ']r',  '<Plug>(coc-references)')
 vim.keymap.set('n', ']d',  '<Plug>(coc-definition)')
 vim.keymap.set('v', ']f', '<Plug>(coc-format-selected)')
 vim.keymap.set('n', '<c-p>', '<Plug>(PickerEdit)')
-vim.keymap.set('n', ']q', ':cnext<cr>')
-vim.keymap.set('n', '[q', ':cprevious<cr>')
 vim.keymap.set('i', ';', '<esc>mzA;<esc>`z')
 vim.keymap.set('t', '<esc>', '<c-\\><c-n>')
 vim.keymap.set('n', 'K', ':call CocAction("doHover")<cr>')
-
 vim.cmd('packadd cfilter')
 vim.cmd("call coc#add_extension('coc-pyright', 'coc-json', 'coc-git', 'coc-phpls', 'coc-css', 'coc-html')")
 
