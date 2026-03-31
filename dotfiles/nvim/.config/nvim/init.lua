f32de43804537398119584efc745697ef4795ad6
Joe Reynolds <joereynolds952@gmail.com>
2026-03-31 09:09:53 +0100

    Adjust init.vim for 0.12

--- dotfiles/nvim/.config/nvim/init.lua
+++ dotfiles/nvim/.config/nvim/init.lua
@@ -1,13 +1,14 @@
-local Plug = vim.fn['plug#']
+vim.pack.add({
+    'https://github.com/machakann/vim-sandwich',
+    { src = 'https://github.com/saghen/blink.cmp', version = "v1.6.0" },
+    'https://github.com/srstevenson/vim-picker',
+    'https://github.com/stefandtw/quickfix-reflector.vim',
+    'https://github.com/tpope/vim-fugitive',
+    'https://github.com/michaelb/sniprun',
+})
 
-vim.call('plug#begin')
-    Plug 'machakann/vim-sandwich'
-    Plug ('saghen/blink.cmp', { tag = '*' })
-    Plug 'srstevenson/vim-picker'
-    Plug 'stefandtw/quickfix-reflector.vim'
-    Plug 'tpope/vim-fugitive'
-    Plug('michaelb/sniprun', { ['do'] = 'sh ./install.sh' })
-vim.call('plug#end')
+vim.cmd.packadd('cfilter')
+vim.cmd.packadd('nvim.undotree')
 
 vim.lsp.enable('intelephense') -- ~/.config/nvim/lsp/intelephense.lua
 vim.lsp.enable('pylsp') -- ~/.config/nvim/lsp/pylsp.lua
@@ -19,7 +20,6 @@ vim.opt.shiftwidth = 4
 vim.opt.swapfile = false
 vim.keymap.set('n', '<c-p>', '<Plug>(PickerEdit)')
 vim.keymap.set('i', ';', '<esc>mzA;<esc>`z')
-vim.cmd('packadd cfilter')
 vim.diagnostic.config({ virtual_text = true})
 vim.keymap.set('n', '<c-c><c-c>', function() require('sniprun').run() end, { silent = true })
 
