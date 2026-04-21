5a8d7430777bc0906962ae1aea70b8503da55daf
Joe Reynolds <joereynolds952@gmail.com>
2026-04-21 11:25:11 +0100

    Remove sniprun, just use tmux

--- dotfiles/nvim/.config/nvim/init.lua
+++ dotfiles/nvim/.config/nvim/init.lua
@@ -1,10 +1,9 @@
 vim.pack.add({
     'https://github.com/machakann/vim-sandwich',
-    { src = 'https://github.com/saghen/blink.cmp', version = "v1.6.0" },
+    { src = 'https://github.com/saghen/blink.cmp', version = "v1" },
     'https://github.com/srstevenson/vim-picker',
     'https://github.com/stefandtw/quickfix-reflector.vim',
     'https://github.com/tpope/vim-fugitive',
-    'https://github.com/michaelb/sniprun',
 })
 
 vim.cmd.packadd('cfilter')
@@ -19,7 +18,5 @@ vim.opt.swapfile = false
 vim.keymap.set('n', '<c-p>', '<Plug>(PickerEdit)')
 vim.keymap.set('i', ';', '<esc>mzA;<esc>`z')
 vim.diagnostic.config({ virtual_text = true})
-vim.keymap.set('n', '<c-c><c-c>', function() require('sniprun').run() end, { silent = true })
 
-require('sniprun').setup({display = { "TempFloatingWindow" }})
 require('blink.cmp').setup()
